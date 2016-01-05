089034fc8a3c77e7620c0d81e9ca4896ef9825cb
Melanie Burger <melanie.brgr@gmail.com>
2016-01-04 21:14:40 -0500

    input onBlur function

--- js/app.js
+++ js/app.js
@@ -18,31 +18,52 @@ $.getJSON("js/items.json")
 function setPredictiveType() {
 	$('.list__product').keyup(function(e) {
 		var that = this;
-		var val = $('.list__product input').val();
+		var val = $(this).find('input').val();
 
 		if ( val.length > 0 ) { 
-			$(that).siblings('ul')
+			$(this).siblings('ul')
 				.empty()
 				.addClass('is-matched') //only needs to happen when element is focussed
 				.css('display', 'block'); //only needs to happen when element is focussed
 
-			items.forEach(function(el, i, arr) {
+			items.forEach(function(el) {
 				if ( el.product.indexOf(val) > -1 ) { 
 					$(that).siblings('ul').append('<li>' + el.product + '</li>');
 				}
 			});
-			
-			//if there were no matches (no list items)
-			log($(that).siblings('ul').length);
-			// $(that).siblings('ul').append('<li> No matches :( </li>');
+			if ( !$(this).siblings('ul').children().length ) { //remove if empty
+				$(this).siblings('ul')
+					.css('display', 'none')
+					.removeClass('is-matched')
+					.empty();
+			}
 		} else {
-				$(that).siblings('ul').css('display', 'none').removeClass('is-matched').empty();
+			$(this).siblings('ul')
+				.css('display', 'none')
+				.removeClass('is-matched')
+				.empty();
 		}
 	});
+}
+
+function setOnBlur() {
+	$('.list__product input').blur(function() {
+		$(this).parent().siblings('ul')
+			.css('display', 'none')
+			.removeClass('is-matched')
+			.empty();
+	});
+}
+
+function enableLineItemClick() {
+	// try without .off() >:)
+	// $('.list__product ul li').off()
+	$('.list__product ul li').click(function(){log('hi');});
 }
 
 /* ----- DOC READY ----- */
 $(document).ready(function() {
 	setPredictiveType();
+	setOnBlur();
 });
 
