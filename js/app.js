17747aeaabef28cc8a811b23425febb37c656b5b
Melanie Burger <melanie.brgr@gmail.com>
2016-01-06 22:48:35 -0500

    price calculation on line click

--- js/app.js
+++ js/app.js
@@ -26,15 +26,38 @@ function setOnBlur() {
 	});
 }
 
+function calculatePrice( $products ) {
+	var price, itemData;
+
+	var $qtyInput = $products.parent().siblings().children('.list__quantity').children('input');
+	var $priceInput = $products.parent().siblings().children('.list__price').children('input');
+	var $prodInput = $products.children('input');
+	var product = $prodInput.val();
+	var quantity = $qtyInput.val() || 1;
+
+	//find or retrieve matched product
+	items.forEach(function(el) {
+		if ( el.product === product ) { 
+			itemData = el;
+			price = el.ppUnit*quantity;
+			$priceInput.val(price);
+			return;
+		}
+	});
+	// log(price);
+}
+
 function enableLineItemClick( $itemsList ) {
 	$itemsList.find('li').off();
 	$itemsList.find('li').click(function() {
+		log('test');
 		var $products = $(this).parent().siblings('.list__product');
 		$products.children('input').val( $(this).text() );
 		$products.siblings('ul')
 			.css('display', 'none')
 			.removeClass('is-matched')
 			.empty();
+		calculatePrice( $products );
 	});
 }
 
@@ -49,11 +72,10 @@ function setPredictiveType() {
 				.addClass('is-matched') //only needs to happen when element is focussed
 				.css('width', $(this).parent().width())
 				.css('display', 'block'); //only needs to happen when element is focussed
-
 			items.forEach(function(el) {
 				if ( el.product.indexOf(val) > -1 ) {
 					var firstMatchingCharIndex = el.product.indexOf(val);
-					var lastMatchingCharIndex = val.length - 1;
+					var lastMatchingCharIndex = firstMatchingCharIndex + val.length - 1;
 					var productCharArr = el.product.split('');
 					var highlightedStr = '';
 					productCharArr.forEach(function(el, i) { //reformat prodcut strng with new class
