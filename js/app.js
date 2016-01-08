7f5323747ef0b069c9e7791babcf97ea3e7065ef
Melanie Burger <melanie.brgr@gmail.com>
2016-01-08 00:23:24 -0500

    slight refactor of setPredictiveType()

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 function log(m) {
 	console.log(m);
 }
+log('shopping list app');
 
 /* ----- APP ----- */
 var items;
@@ -26,6 +27,13 @@ function setOnBlur() {
 	});
 }
 
+// function calculateTotal() {
+// 	log('calculateTotal() fired');
+// 	$('list__price input').each(function() {
+// 		log('each');
+// 	});
+// }
+
 function calculatePrice( $row ) {
 	var quantity, product, price, itemData;
 	var $qtyInput = $row.find('.list__quantity').children('input');
@@ -45,6 +53,7 @@ function calculatePrice( $row ) {
 			itemData = el;
 			price = el.ppUnit*quantity;
 			$priceInput.val(price);
+			// calculateTotal();
 			return;
 		}
 	});
@@ -52,6 +61,7 @@ function calculatePrice( $row ) {
 
 function clearPrice( $row ) {
 	$row.find('.list__price').children('input').val('');
+	// calculateTotal();
 }
 
 function setQuantity() {
@@ -87,21 +97,25 @@ function setPredictiveType() {
 	$('.list__product input').keyup(function() {
 		var $row = $(this).closest('div[class^=row]');
 		var $itemsList = $(this).parent().siblings('ul'); //reference to product list
-		var val = $(this).val(); //value of product text field
+		var product = $(this).val(); //value of product text field
+		function closeItemsList() {
+			$itemsList.css('display', 'none').removeClass('is-matched').empty(); 
+		}
 
-		if ( val.length > 0 ) { 
-			$itemsList.empty().addClass('is-matched').css('width', $(this).parent().width()).css('display', 'block');
+		if ( product.length > 0 ) { 
+			$itemsList.empty();
+
 			items.forEach(function(el) {
-				// if ( el.product ===  )
-				if ( el.product.indexOf(val) > -1 ) { //find matching characters
-					var firstMatchingCharIndex = el.product.indexOf(val);
-					var lastMatchingCharIndex = firstMatchingCharIndex + val.length - 1;
+				if ( el.product.indexOf(product) > -1 ) {
+					$itemsList.addClass('is-matched').css('width', $(this).parent().width()).css('display', 'block');
+					var firstMatchingCharIndex = el.product.indexOf(product);
+					var lastMatchingCharIndex = firstMatchingCharIndex + product.length - 1;
 					var productCharArr = el.product.split('');
 					var highlightedStr = '';
-					productCharArr.forEach(function(el, i) { //format string
+					productCharArr.forEach(function(el, i) {
 						if ( i === (firstMatchingCharIndex) ) {
 							highlightedStr += '<span class="is-matching-char">' + el;
-							if ( val.length === 1 ) { highlightedStr += '</span>'; }
+							if ( product.length === 1 ) { highlightedStr += '</span>'; }
 						} else if ( i === (lastMatchingCharIndex) ) {
 							highlightedStr += el + '</span>';
 						} else {
@@ -110,13 +124,24 @@ function setPredictiveType() {
 					});
 					$itemsList.append('<li>' + highlightedStr + '</li>');
 				}
+				if ( el.product === product ) {
+					calculatePrice( $row );
+					// closeItemsList(); //wierd behaviour if close on match
+					return;
+				}
 			});
-			if ( $itemsList.children().length ) { enableLineItemClick( $itemsList ); //attach click handler to items
-			} else { $itemsList.css('display', 'none').removeClass('is-matched').empty(); } //hide list
+
+			if ( $itemsList.children().length ) { 
+				enableLineItemClick( $itemsList ); //attach click handler to items
+			} else { 
+				closeItemsList();
+				clearPrice( $row );
+			}
+
 		} else { 
-			$itemsList.css('display', 'none').removeClass('is-matched').empty(); 
+			closeItemsList();
 			clearPrice( $row );
-		} //hide list
+		}
 
 	});
 }
